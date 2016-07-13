c552ed01e7fdb2f6aad428b39f3b18fed42b4e65
liuzhonghua <471509014@qq.com>
2016-07-13 11:05:30 +0800

    Initial Commit

---
+++ EMSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EMSDemo
+//
+//  Created by LIANGKUNLIN on 16/7/13.
+//  Copyright © 2016年 meilixun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EMSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EMSDemo
+//
+//  Created by LIANGKUNLIN on 16/7/13.
+//  Copyright © 2016年 meilixun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
